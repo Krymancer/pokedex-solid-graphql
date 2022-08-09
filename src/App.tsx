226a026b69de98cdc3bc496ec563038b3e17cc01
Júnior Nascimento <junior.nascm@gmail.com>
2022-08-09 10:00:23 -0300

    POC of getting dominant color of pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-import type {Component} from 'solid-js';
+import {Component, createSignal} from 'solid-js';
+
+import getAverageRGB, {rgbToHex} from '@utils/color';
 
 const App: Component = () => {
+  const [color, setColor] = createSignal('#f00');
+  const imageUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png';
+  let img;
+
+  function changeColor() {
+    const color = getAverageRGB(img);
+    const hexColor = rgbToHex(...color);
+    setColor(hexColor);
+  }
+
   return (
-    <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
+    <div class="h-screen w-screen flex items-center justify-center">
+      <div style={{'background-color': `#${color()}`}} class="w-full h-full justify-center items-center flex">
+        <img src={imageUrl} ref={img} crossOrigin="anonymous" onLoad={changeColor} />
+      </div>
+    </div>
   );
 };
 
